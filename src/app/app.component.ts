63b012a900f23a6cf418db6b2339296399493ef4
kalu77 <kalujerovich77@hotmail.com>
2021-03-16 20:05:02 -0300

    Crud Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from './services/employee/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'employeefront';
+export class AppComponent implements OnInit {
+  employeeForm!: FormGroup;
+  employee: any;
+
+  constructor(
+    public fb: FormBuilder,
+    public employeeService: EmployeeService
+  ) {}
+
+  ngOnInit(): void {
+    this.employeeForm = this.fb.group({
+      id: [''],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      hireDate: ['', Validators.required],
+      salary: ['', Validators.required],
+      commissionPct: ['', Validators.required],
+    });
+
+    this.employeeService.getAllEmployee().subscribe(
+      (resp) => {
+        this.employee = resp;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  guardar(): void {
+    this.employeeService.saveEmployee(this.employeeForm.value).subscribe(
+      (resp) => {
+        this.employeeForm.reset();
+        this.employee=this.employee.filter(employee=> resp.id!==employee.id);
+        this.employee.push(resp);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  eliminar(employee){
+    this.employeeService.deleteEmployee(employee.id).subscribe(resp=>{
+      console.log(resp);
+      if(resp===true){
+        this.employee.pop(employee)
+      }
+    })
+  }
+
+  editar(employee){
+    this.employeeForm.setValue({
+      id:employee.id,
+      firstName: employee.firstName,
+      lastName: employee.lastName,
+      email: employee.email,
+      phoneNumber: employee.phoneNumber,
+      hireDate:employee.hireDate,
+      salary: employee.salary,
+      commissionPct: employee.commissionPct,
+    })
+  }
+
+
 }
---
+++ src/app/services/employee/employee.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private API_SERVER = "http://localhost:8080/employee/";
+
+  constructor(private httpClient: HttpClient) { }
+
+
+
+  public getAllEmployee(): Observable<any>{
+    return this.httpClient.get(this.API_SERVER);
+  }
+
+  public saveEmployee (employee:any): Observable<any>{
+    return this.httpClient.post(this.API_SERVER,employee);
+  }
+
+  public deleteEmployee(id: string):Observable<any>{
+    return this.httpClient.delete(this.API_SERVER + "delete/"+id);
+  }
+
+
+}
